bbc03b9f7c7ec9884b672ef28f88655ae15949bc
Brian Birtles <birtles@gmail.com>
2023-11-01 21:00:24 +0900

    wip: add random notes about next steps

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { RendererActionSlotProps } from 'react-cosmos-ui';
-import { createPlugin } from 'react-plugin';
+import type {
+  RendererActionSlotProps,
+  RendererCoreSpec,
+} from 'react-cosmos-ui';
+import { type PluginContext, createPlugin } from 'react-plugin';
 
 import { DarkModeButton } from './DarkModeButton.js';
 import { DarkModeSpec } from './spec.js';
@@ -9,11 +12,28 @@ const { namedPlug, register } = createPlugin<DarkModeSpec>({
   name: 'darkMode',
 });
 
-// XXX Not sure if the below is supposed to "darkMode" or the name of another
-// button we want to be placed after.
-namedPlug<RendererActionSlotProps>('rendererAction', 'darkMode', () => {
-  return <DarkModeButton onClick={() => {}} />;
-});
+// XXX Need to work out how to forcibly register a dark mode decorator and then
+// have that decorator read from the fixture state like here:
+//
+// https://cosmos-docs-woad.vercel.app/docs/plugins/fixture-plugins
+
+namedPlug<RendererActionSlotProps>(
+  'rendererAction',
+  'darkMode',
+  ({ pluginContext }) => {
+    // XXX Read the darkMode state from rendererCore
+
+    return (
+      <DarkModeButton
+        onClick={() => {
+          // XXX Store the darMode state in storage
+          // setDarkModeState(pluginContext, { ...darkModeState, mode: ??? });
+          setFixtureDarkModeState(pluginContext, 'dark');
+        }}
+      />
+    );
+  }
+);
 
 export { register };
 
@@ -21,4 +41,16 @@ declare const process: unknown;
 
 if (typeof process !== 'object' || (process as any).env.NODE_ENV !== 'test') {
   register();
+}
+
+function setFixtureDarkModeState(
+  context: PluginContext<DarkModeSpec>,
+  mode: 'dark' | 'light' | null
+) {
+  const { getMethodsOf } = context;
+  const rendererCore = getMethodsOf<RendererCoreSpec>('rendererCore');
+  rendererCore.setFixtureState((fixtureState) => ({
+    ...fixtureState,
+    mode,
+  }));
 }
