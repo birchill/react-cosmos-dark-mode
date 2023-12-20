8e894faefc719ac993aca3f3fe80f5292147c85a
Brian Birtles <birtles@gmail.com>
2023-12-20 14:29:07 +0900

    feat: show system state on icon

--- src/DarkModeButton.tsx
+++ src/DarkModeButton.tsx
@@ -9,14 +9,38 @@ type Props = {
 };
 
 export function DarkModeButton(props: Props) {
-  // TODO: Highlighted state
+  // I tried for about an hour to do the following styling with
+  // styled-components but I couldn't get it to work. I believe we need to use
+  // the same instance of styled-components as the Cosmos UI package so that we
+  // can extend `IconButton32` in the usual way (`styled(IconButton32)`), but
+  // due to the way this module is dynamically loaded, I couldn't get that to
+  // work.
   return (
-    <IconButton32
-      icon={<MoonIcon filled={props.filled} />}
-      selected={props.selected}
-      title="Toggle dark mode"
-      onClick={props.onClick}
-    />
+    <div style={{ display: 'grid' }}>
+      <div style={{ gridArea: '1 / 1 / -1 / -1' }}>
+        <IconButton32
+          icon={<MoonIcon filled={props.filled} />}
+          selected={props.selected}
+          title="Toggle dark mode"
+          onClick={props.onClick}
+        />
+      </div>
+      {props.highlighted && (
+        <div
+          style={{
+            gridArea: '1 / 1 / -1 / -1',
+            width: '6px',
+            height: '3px',
+            borderTopLeftRadius: '3px',
+            borderTopRightRadius: '3px',
+            background: '#28b486',
+            alignSelf: 'end',
+            justifySelf: 'center',
+            boxShadow: '0 -1px 3px 1px rgba(40, 180, 134, 0.58)',
+          }}
+        />
+      )}
+    </div>
   );
 }
 
