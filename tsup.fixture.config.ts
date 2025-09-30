91eea6f9a2d8e99883ce7a73880704c8dca22d4a
Brian Birtles <birtles@gmail.com>
2025-09-30 11:19:11 +0900

    chore: switch to tsdown

--- tsup.fixture.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  dts: true,
-  entry: ['src/DarkMode.tsx'],
-  external: ['react', 'react-cosmos/client'],
-  format: ['cjs', 'esm'],
-  splitting: false,
-});
